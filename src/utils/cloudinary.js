70121c9984cdf028fc14333c51c5fe5ae6ac86b4
vishaldevasics <vishaldevasics@gmail.com>
2024-08-16 22:30:50 +0530

    fixed all register controller bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,14 +1,11 @@
 import {v2 as cloudinary} from "cloudinary"
-
-import { v2 as cloudinary } from 'cloudinary';
-
 import fs from "fs"
 
 // Configuration
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+    api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
 });
 
 
@@ -19,10 +16,8 @@ const uploadOnCloudinary = async (localFilePath) =>{
         const response = await cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
-
-        console.log("file is uploaded on cloudinary ");
-        response.url();
-        return response
+        fs.unlinkSync(localFilePath)
+        return response;
 
 
     } catch (error) {
